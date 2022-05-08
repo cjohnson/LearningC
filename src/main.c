4a314e81dbf6a050241dbac1dc7f181cb266bfd5
cjohnson <collin.g.johnson@gmail.com>
2022-05-08 18:52:53 -0400

    Basic fahrenheit to celsius conversion example!

--- src/main.c
+++ src/main.c
@@ -2,5 +2,10 @@
 
 main()
 {
-    printf("hello, world!\n");
+    int celsius;
+    for(int fahr = 0; fahr <= 300; fahr += 20)
+    {
+        celsius = 5 * (fahr - 32) / 9;
+        printf("%d\t%d\n", fahr, celsius);
+    }
 }
