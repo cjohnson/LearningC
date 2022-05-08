6ace776afb37dc6bbcf839f803756b9dead258af
cjohnson <collin.g.johnson@gmail.com>
2022-05-08 19:13:23 -0400

    Basic getchar inputs

--- src/main.c
+++ src/main.c
@@ -2,10 +2,12 @@
 
 int main()
 {
-    float celsius;
-    for(float fahr = 0.0; fahr <= 300.0; fahr += 20.0)
+    int c;
+
+    c = getchar();
+    while (c != EOF)
     {
-        celsius = (5.0/9.0) * (fahr - 32.0);
-        printf("%3.0f %6.1f\n", fahr, celsius);
+        putchar(c);
+        c = getchar();
     }
 }
